026e77f80ed46770f6860efb73e40baea37a5be2
Kevin <paperdev0@gmail.com>
2023-08-16 15:59:25 +0900

    drag and drop

--- src/app/components/history.tsx
+++ src/app/components/history.tsx
@@ -1,25 +1,53 @@
 'use client';
 
+import { useState } from 'react';
 import { dataHistory, iHistory } from '@/data/history';
+import { DragDropContext, Droppable, Draggable, DropResult } from 'react-beautiful-dnd';
 
 const History: React.FC<iHistory> = ({ companyName, contents }) => {
+  const [dataContents, updateContents] = useState(contents);
+
+  const onDragEnd = (result: DropResult) => {
+    if (!result.destination) {
+      return;
+    };
+
+    const items = Array.from(dataContents);
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
+    updateContents(items);
+  }
+
   return (
     <div>
-      <p className="my-2 ml-2 text-blue-500 text-base">
-        {companyName}
-      </p>
-      {contents.map((content, index) => {
-        return (
-          <div key={index} className='max-w-md mx-auto rounded-xl'>
-            <p className="my-2 ml-2 text-base/2 text-black-500">
-              {content.beginDate} ~ {content.endDate}
-            </p>
-            <p className="my-2 mx-2 text-slate-500">
-              {content.description}
-            </p>
-          </div>
-        );
-      })}
+      <DragDropContext onDragEnd={onDragEnd}>
+        <Droppable droppableId='droppableHistory'>
+          {(provided, snapshot) => (
+            <div {...provided.droppableProps} ref={provided.innerRef}>
+              {dataContents.map((content, index) => {
+                return (
+                  <Draggable key={index} draggableId={index+''} index={index}>
+                    {(provided, snapshot) => (
+                      <div className='max-w-md mx-auto rounded-xl' ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                        <p className="my-2 ml-2 text-blue-500 text-base">
+                          {companyName}
+                        </p>
+                        <p className="my-2 ml-2 text-base/2 text-black-500">
+                          {content.beginDate} ~ {content.endDate}
+                        </p>
+                        <p className="my-2 mx-2 text-slate-500">
+                          {content.description}
+                        </p>
+                      </div>
+                    )}
+                  </Draggable>
+                );
+              })}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
     </div>
   );
 };
