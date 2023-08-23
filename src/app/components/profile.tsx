526912835c395a50872981a57ef1daa52e6d4a58
Kevin <paperdev0@gmail.com>
2023-08-23 12:35:24 +0900

    icon

--- src/app/components/profile.tsx
+++ src/app/components/profile.tsx
@@ -5,6 +5,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { LuGithub, LuMail } from 'react-icons/lu';
 
 let isToast = false;
 const delayTime = 2000;
@@ -35,22 +36,27 @@ export default async function ComponentProfile({
   return (
     <div className='py-8 px-8 max-w-md mx-auto rounded-xl shadow-md md:max-w-2xl space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6'>
       <Image className='block mx-auto h-24 rounded-full sm:mx-0 sm:flex-shrink-0' src={dataProfile.avatar} width={100} height={100} alt={dataProfile.name}/>
+      
       <div className='text-center space-y-2 sm:text-left'>
         <div className='space-y-0.5'>
-          <p className='text-lg text-black font-semibold'>
+
+          <p className='text-lg font-semibold'>
             {dataProfile.name}
           </p>
+
           <p className='text-gray-500 font-medium'>
             {dataProfile.jobTitle}
           </p>
-          <p>
-            <Link href={dataProfile.github} target='_blank' className='text-sm text-purple-600 font-semibold hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2'>
-              {dataProfile.github}
+          
+          <p className='paper-join text-accent pt-2'>
+            <Link className='paper-tooltip' data-tip='click to open' href={dataProfile.github} target='_blank'>
+                <LuGithub className='w-6 h-6'></LuGithub>
             </Link>
+            <button className='px-2 paper-tooltip' data-tip='click to copy' onClick={() => {copyText(dataProfile.email)}}>
+              <LuMail className='w-6 h-6'></LuMail>
+            </button>
           </p>
-          <button onClick={() => {copyText(dataProfile.email)}} title='click to copy'>
-            {dataProfile.email}
-          </button>
+
           <ToastContainer/>
         </div>
       </div>
