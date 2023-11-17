c1fe6a78546b398e206d0fdbddb47b75ac4cf244
Kevin <paperdev0@gmail.com>
2023-11-17 13:22:41 +0900

    profile fixed

--- client/src/components/(profile)/profile.tsx
+++ client/src/components/(profile)/profile.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import React from "react";
-import {Avatar} from "@nextui-org/react";
+import React from 'react';
+import { Avatar } from '@nextui-org/react';
 import Link from 'next/link';
 import { toast, ToastContainer } from 'react-toastify';
 import { LuGithub, LuMail, LuLinkedin } from 'react-icons/lu';
@@ -34,8 +34,8 @@ export default async function ComponentProfile({
   dataProfile: iProfile
 }) {
   return (
-    <div className='flex flex-col justify-center items-center text-center sm:flex sm:flex-row sm:justify-center sm:items-center sm:py-4 mx-auto rounded-xl shadow-md'>
-      <Avatar src={dataProfile.avatar} className="w-24 h-24"></Avatar>
+    <div className='flex flex-col justify-center items-center text-center sm:flex sm:flex-row sm:justify-center sm:items-center sm:text-left sm:py-4 mx-auto rounded-xl shadow-md'>
+      <Avatar src={dataProfile.avatar} className='w-24 h-24'></Avatar>
 
       <div className='sm:pl-4'>
         <div className='py-2'>
@@ -46,20 +46,20 @@ export default async function ComponentProfile({
           <div className='font-medium'>
             {dataProfile.jobTitle}
           </div>
-          
-          <div className='flex space-x-2 mt-2'>
+
+          <div className='flex gap-2 mt-2 justify-center sm:justify-start'>
             <Link href={dataProfile.github} target='_blank'>
-                <LuGithub className='w-6 h-6'></LuGithub>
+              <LuGithub className='w-6 h-6'></LuGithub>
             </Link>
             <Link href={dataProfile.linkedin} target='_blank'>
-                <LuLinkedin className='w-6 h-6'></LuLinkedin>
+              <LuLinkedin className='w-6 h-6'></LuLinkedin>
             </Link>
-            <button data-tip='click to copy' onClick={() => {copyText(dataProfile.email)}}>
+            <button data-tip='click to copy' onClick={() => { copyText(dataProfile.email) }}>
               <LuMail className='w-6 h-6'></LuMail>
             </button>
           </div>
 
-          <ToastContainer/>
+          <ToastContainer />
         </div>
       </div>
     </div>
